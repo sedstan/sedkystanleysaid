75ed6d945810f79f7b37c18bc296b970ac3eb52a
Sedky Said <sed@sedkystanleysaid.com>
2021-03-27 09:05:07 +0000

    setup tailwindcss an postcss

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      titillium: ['Titillium Web, sans-serif'],
+      roboto: ['Roboto, sans-serif']
+    },
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        copper: '#a8763e',
+        oldLace: '#f7f3e3',
+        cultured: '#ecf0f1',
+        barnRed: '#6f1a07',
+        bistro: '#2b2118'
+      },
+      backgroundOpacity: {
+        '09': '0.9'
+      }
+    },
+    future: {
+      removeDeprecatedGapUtilities: true
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
