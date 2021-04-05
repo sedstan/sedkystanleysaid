43352070367fdb77b882abee8d12e5e29b481339
Sedky Said <sed@sedkystanleysaid.com>
2021-04-05 16:03:21 +0100

    WIP - hamburger menu

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="lg:hidden bg-barnRed text-oldLace fixed w-full h-14 flex flex-row justify-center items-center">
-    <button id="mobile-menu-btn" class="flex-1">More</button>
+    <div class="full-menu-btn flex-1 relative flex justify-center cursor-pointer transition duration-75 ease-in-out">
+      <div class="full-menu-btn__burger w-5 h-1 bg-oldLace"></div>
+    </div>
     <nav class="flex-auto">
       <ul class="flex flex-row justify-evenly">
         <li v-for="link in links" :key="link.id">
