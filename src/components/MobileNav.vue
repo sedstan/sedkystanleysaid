9b87ed17d0bc6eb8ecc11a739213e4bc8105a149
Sedky Said <sed@sedkystanleysaid.com>
2021-04-09 20:57:55 +0100

    adds emoji's to full-menu

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="lg:hidden bg-barnRed text-oldLace font-black fixed w-full h-14 flex flex-row justify-center items-center">
-    <nav id="full-menu" class="hidden w-full bg-barnRed text-oldLace absolute bottom-14 p-4">
-      <ul>
-        <li v-for="link in links" :key="link.id" class="flex-auto">
-          <router-link v-if="link.id === 2" :to="`${link.page}`" v-on:click="open">{{ link.name }}</router-link>
+    <nav
+      id="full-menu"
+      class="hidden w-full bg-barnRed text-oldLace font-titillium font-black text-2xl absolute bottom-14 p-4 border-b border-oldLace"
+    >
+      <ul class="flex flex-col w-full">
+        <li v-for="link in links" :key="link.id" class="flex-auto border-b border-oldLace">
+          <router-link v-if="link.id === 2" :to="`${link.page}`" v-on:click="open"
+            ><span class="mr-14">&#x1F468;</span>{{ link.name }}</router-link
+          >
         </li>
         <li v-for="link in links" :key="link.id" class="flex-auto">
-          <router-link v-if="link.id === 3" :to="`${link.page}`" v-on:click="open">{{ link.name }}</router-link>
+          <router-link v-if="link.id === 3" :to="`${link.page}`" v-on:click="open"
+            ><span class="mr-14">&#x1F3C6;</span>{{ link.name }}</router-link
+          >
         </li>
         <li v-for="link in links" :key="link.id" class="flex-auto">
-          <router-link v-if="link.id === 4" :to="`${link.page}`" v-on:click="open">{{ link.name }}</router-link>
+          <router-link v-if="link.id === 4" :to="`${link.page}`" v-on:click="open"
+            ><span class="mr-14">&#x1F44B;</span>{{ link.name }}</router-link
+          >
         </li>
       </ul>
     </nav>
