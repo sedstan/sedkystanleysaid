6b393b5b10d040ebedd7ba847ab307b131a53b67
Sedky Said <sed@sedkystanleysaid.com>
2021-04-10 19:23:48 +0100

    Updates to v-for directive --uses computed in place of v-if for rendering conditional elements

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -1,36 +1,30 @@
 <template>
-  <div class="lg:hidden bg-barnRed text-oldLace font-black fixed w-full h-14 flex flex-row justify-center items-center">
+  <div
+    class="lg:hidden bg-barnRed text-oldLace font-black fixed w-full h-14 flex flex-row justify-center items-center align-center"
+  >
     <nav
       id="full-menu"
       class="hidden w-full bg-barnRed text-oldLace font-titillium font-black text-2xl absolute bottom-14 p-4 border-b border-oldLace"
     >
       <ul class="flex flex-col w-full">
-        <li class="nav__item--bottom-border flex-auto" v-for="link in links" :key="link.id">
-          <router-link
-            class="border-b border-oldLace"
-            v-if="link.id === 2 || link.id === 3 || link.id === 4"
-            :to="`${link.page}`"
-            v-on:click="open"
+        <li class="nav__item--bottom-border flex-auto" v-for="link in moreMenu" :key="link.id">
+          <router-link class="border-b border-oldLace" :to="`${link.page}`" v-on:click="open"
             >{{ link.emoji }}{{ link.name }}</router-link
           >
         </li>
       </ul>
     </nav>
-    <nav class="flex-auto w-2/3">
-      <ul class="flex flex-row justify-around">
-        <li class="w-full" v-for="link in links" :key="link.id">
-          <router-link class="flex-auto w-full" v-if="link.id === 0 || link.id === 1" :to="`${link.page}`"
+    <nav class="flex-auto">
+      <ul class="flex flex-row justify-between">
+        <li class="w-full" v-for="link in navbarLinks" :key="link.id">
+          <router-link class="flex-auto w-full" :to="`${link.page}`"
             ><span class="text-2xl">{{ link.emoji }}</span
-            ><br /><span class="text-xs">{{ link.name }}</span></router-link
+            ><br /><span class="text-xs font-black">{{ link.name }}</span></router-link
           >
         </li>
       </ul>
     </nav>
-    <button
-      id="menu-btn"
-      class="inline-block cursor-pointer flex-auto text-base focus:outline-none focus:ring-0"
-      @click="open"
-    >
+    <button id="menu-btn" class="flex-auto cursor-pointer text-base focus:outline-none focus:ring-0" @click="open">
       &#x2022; &#x2022; &#x2022;
     </button>
   </div>
@@ -53,35 +47,57 @@ export default {
       links: [
         {
           id: 0,
-          emoji: '\ud83c\udfe0',
           name: 'Home',
-          page: '/'
+          page: '/',
+          emoji: '\ud83c\udfe0',
+          navbar: true,
+          moreMenu: false
         },
         {
           id: 1,
           emoji: '\ud83d\udcbc',
           name: 'Projects',
-          page: '/#projects'
+          page: '/#projects',
+          navbar: true,
+          moreMenu: false
         },
         {
           id: 2,
           emoji: '\ud83d\udc68',
           name: 'About Me',
-          page: '/#about-me'
+          page: '/#about-me',
+          navbar: false,
+          moreMenu: true
         },
         {
           id: 3,
           emoji: '\ud83c\udfc6',
           name: 'Achievements',
-          page: '/#achievements'
+          page: '/#achievements',
+          navbar: false,
+          moreMenu: true
         },
         {
           id: 4,
           emoji: '\ud83d\ude4b',
           name: 'Be Social',
-          page: '/#be-social'
+          page: '/#be-social',
+          navbar: false,
+          moreMenu: true
         }
       ]
+    }
+  },
+  computed: {
+    navbarLinks: function () {
+      return this.links.filter(function (l) {
+        return l.navbar
+      })
+    },
+    moreMenu: function () {
+      return this.links.filter(function (l) {
+        return l.moreMenu
+      })
     }
   }
 }
