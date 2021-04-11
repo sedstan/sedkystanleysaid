8c4d106f182c18c2366de2859b67662a72e89894
Sedky Said <sed@sedkystanleysaid.com>
2021-04-11 01:10:17 +0100

    restructures desktop nav. separates out home link

--- src/components/DesktopNav.vue
+++ src/components/DesktopNav.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="hidden lg:container lg:flex lg:justify-center">
-    <ul>
-      <li v-for="link in links" :key="link.id" class="flex-auto">
+  <nav class="hidden lg:container lg:flex lg:justify-end lg:w-10/12">
+    <ul class="lg:flex-row">
+      <li v-for="link in notHomeLinks" :key="link.id" class="flex-auto">
         <router-link :to="`${link.page}`"
           ><span class="text-lg">{{ link.emoji }}</span
           ><span class="text-xs font-black">{{ link.name }}</span></router-link
@@ -19,29 +19,47 @@ export default {
       links: [
         {
           id: 0,
-          emoji: '\ud83d\udcbc',
-          name: 'Projects',
-          page: '/#projects'
+          name: 'Home',
+          page: '/',
+          emoji: '\ud83c\udfe0',
+          notHomeLink: false
         },
         {
           id: 1,
-          emoji: '\ud83d\udc68',
-          name: 'About Me',
-          page: '/#about-me'
+          emoji: '\ud83d\udcbc',
+          name: 'Projects',
+          page: '/#projects',
+          notHomeLink: true
         },
         {
           id: 2,
-          emoji: '\ud83c\udfc6',
-          name: 'Achievements',
-          page: '/#achievements'
+          emoji: '\ud83d\udc68',
+          name: 'About Me',
+          page: '/#about-me',
+          notHomeLink: true
         },
         {
           id: 3,
+          emoji: '\ud83c\udfc6',
+          name: 'Achievements',
+          page: '/#achievements',
+          notHomeLink: true
+        },
+        {
+          id: 4,
           emoji: '\ud83d\ude4b',
           name: 'Be Social',
-          page: '/#be-social'
+          page: '/#be-social',
+          notHomeLink: true
         }
       ]
+    }
+  },
+  computed: {
+    notHomeLinks: function () {
+      return this.links.filter(function (l) {
+        return l.notHomeLink
+      })
     }
   }
 }
