aa6f9e965bfb2c8b0b55ed47399bf09e03899e02
Sedky Said <sed@sedkystanleysaid.com>
2021-04-23 19:56:11 +0100

    tweak styling

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="flex justify-center align-center flex-col items-center">
-    <img class="" src="https://sed-stan-personal-site.s3.amazonaws.com/images/laptop-stickers-1920.png" />
-    <div class="container absolute font-titillium text-oldLace">
-      <h1 class="font-black text-3xl md:text-6xl lg:text-7xl">{{ name }}</h1>
+    <img src="https://sed-stan-personal-site.s3.amazonaws.com/images/laptop-stickers-1920.png" />
+    <div class="container absolute font-titillium text-oldLace pb-12">
+      <img
+        class="border border-barnRed rounded-full w-36 h-36 mx-auto mb-9"
+        src="https://sed-stan-personal-site.s3.amazonaws.com/images/head-shot-transparent.png"
+      />
+      <h1 class="font-black text-3xl md:text-6xl lg:text-7xl mb-3">{{ name }}</h1>
       <h2 class="font-bold text-2xl md:text-4xl lg:text-6">{{ title }}</h2>
     </div>
   </div>
