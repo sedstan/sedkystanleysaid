44704755fa041f872a916196ae3765bffacef8d2
Sedky Said <sed@sedkystanleysaid.com>
2021-04-30 07:47:36 +0100

    manually in stall vue-apollo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { createApp } from 'vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/index.css'
 
-createApp(App).use(router).use(router).mount('#app')
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:3020/graphql'
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+createApp(App({ apolloClient })).use(router).use(router).mount('#app')
