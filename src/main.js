7a9d88d7ff018e57f6afef23390b9c63ed43dc8d
Sedky Said <sed@sedkystanleysaid.com>
2021-05-03 18:37:34 +0100

    revert main.js to original state.|

--- src/main.js
+++ src/main.js
@@ -1,26 +1,6 @@
 import { createApp, provide, h } from 'vue'
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/index.css'
 
-const defaultClient = new ApolloClient({
-  url: 'https://api.github.com/graphql',
-  cache: new InMemoryCache(),
-  headers: {
-    authroization: `Bearer ${process.env.VUE_APP_SEDKYSTANLEYSAID_GITHUB_TOKEN} `,
-  },
-})
-
-createApp({
-  setup() {
-    provide(DefaultApolloClient, defaultClient)
-  },
-  render() {
-    return h(App)
-  },
-})
-  .use(router)
-  .use(router)
-  .mount('#app')
+createApp(App).use(router).use(router).mount('#app')
