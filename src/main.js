63a54bbd99014c528c1636a6fd78514b4208a7f5
Sedky Said <sed@sedkystanleysaid.com>
2021-05-03 19:53:20 +0100

    i don't like apollo client

--- src/main.js
+++ src/main.js
@@ -1,43 +1,6 @@
-import { createApp } from 'vue'
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
-import { setContext } from '@apollo/client/link/context';
-import { provide } from '@vue/composition-api'
-import { DefaultApolloClient } from '@vue/apollo-composable'
+import { createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/index.css'
 
-const httpLink = createHttpLink({
-  // You should use an absolute URL here
-  uri: 'https://api.github.com/graphql',
-})
-const cache = new InMemoryCache()
-const setAuthorizationLink = setContext((request, previousContext) => ({
-  header: { authorization: `Bearer ${VUE_APP_SEDKYSTANLEYSAID_GITHUB_TOKEN}` }
-}))
-
-const asyncAuthLink = setContext(
-  request => new Promise((success, fail) => {
-    setTimeout(() => {
-      successs({ token: 'async token found' })
-    }, 10)
-  })
-)
-
-
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
-  setAuthorizationLink,
-  asyncAuthLink
-})
-
-
-
-createApp({
-  setup() {
-    provide(DefaultApolloClient, apolloClient)
-  },
-  render: h => h(App)
-
-}).use(router).use(router).mount('#app')
+createApp(App).use(router).use(router).mount('#app')
