080b7a72c0dc71a92c076f2e396d256d1b7c47c2
Sedky Said <sed@sedkystanleysaid.com>
2021-05-08 09:15:48 +0100

    remove vue-integration for @apollo/client

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-import { createApp} from 'vue'
+import { createApp, provide, h } from 'vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/index.css'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+    // You should use an absolute URL here
+    uri: 'http://localhost:3020/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
 
 createApp(App).use(router).use(router).mount('#app')
