937fdb135d1be005c6bc451c1a54ad86c5102489
Sedky Said <sed@sedkystanleysaid.com>
2021-05-08 10:15:03 +0100

    hit github api

---
+++ apollo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    client: {
+        name: 'Sedky Stanley Said',
+        service: VUE_APP_SEDKYSTANLEYSAID_GITHUB_TOKEN
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,23 +1,42 @@
-import { createApp, provide, h } from 'vue'
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
-import { DefaultApolloClient } from '@vue/apollo-composable'
+import { createApp } from 'vue'
+import { ApolloClient, InMemoryCache, gql, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import App from './App.vue'
 import router from './router'
 import './assets/styles/index.css'
 
-// HTTP connection to the API
 const httpLink = createHttpLink({
-    // You should use an absolute URL here
-    uri: 'http://localhost:3020/graphql',
+    uri: 'https://api.github.com/graphql',
 })
 
-// Cache implementation
-const cache = new InMemoryCache()
-
-// Create the apollo client
-const apolloClient = new ApolloClient({
-    link: httpLink,
-    cache,
+const authLink = setContext((_, { headers }) => {
+    const token = process.env.VUE_APP_SEDKYSTANLEYSAID_GITHUB_TOKEN;
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ""
+        }
+    }
 })
+
+const defaultClient = new ApolloClient({
+    cache: new InMemoryCache,
+    link: authLink.concat(httpLink)
+})
+
+const query = gql`
+    query {
+        viewer {
+            name
+            bio
+            avatarUrl
+            email
+        }
+    }
+`
+defaultClient.query({
+    query
+})
+    .then(response => console.log(response))
 
 createApp(App).use(router).use(router).mount('#app')
