9837c3912b7699b8e42f9d4888c32b5151f56a75
Sedky Said <sed@sedkystanleysaid.com>
2021-05-14 23:46:24 +0100

    Make them look more like cards.

--- src/components/ProjectCards.vue
+++ src/components/ProjectCards.vue
@@ -1,17 +1,28 @@
 <template>
     <div v-for="repo in repos" :key="repo.node" class="">
         <app-link isExternal :to="`${repo.node.url}`">
-            <article class="relative flex flex-col justify-center items-center flex-wrap">
+            <article
+                class="relative flex flex-col flex-auto justify-start items-start flex-wrap bg-cultured p-5 border border-solid rounded border-barnRed mx-1 min-w-full w-80 h-auto"
+            >
                 <header>
-                    <h3>{{ repo.node.name }}</h3>
+                    <h3
+                        class="text-3xl md:2xl sm:xl font-titillium text-barnRed"
+                    >
+                        {{ repo.node.name }}
+                    </h3>
                 </header>
-                <main>
-                    <p>{{ repo.node.description }}</p>
+                <main class="relative flex flex-col justify-start items-start flex-wrap">
+                    <p class="font-roboto">{{ repo.node.description }}</p>
                     <div>
-                        <h4>Languages:</h4>
+                        <h4
+                            class="text-2xl md:text-xl sm:text-lg font-titillium"
+                        >
+                            Languages:
+                        </h4>
                         <p
                             v-for="language in repo.node.languages.edges"
                             :key="language.node"
+                            class="font-roboto flex flex-col justify-start items-start"
                         >
                             <span
                                 :style="{ color: `${language.node.color}` }"
@@ -19,9 +30,13 @@
                             >
                         </p>
                     </div>
-                    <div class="hidden">
-                        <h4>Primary Language:</h4>
-                        <p>
+                    <div class="flex flex-col justify-start items-start">
+                        <h4
+                            class="text-2xl md:text-xl sm:text-lg font-titillium"
+                        >
+                            Primary Language:
+                        </h4>
+                        <p class="font-roboto">
                             <span
                                 :style="{
                                     color: `${repo.node.primaryLanguage.color}`,
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,12 +1,12 @@
 <template>
     <section id="#projects" class="bg-copper">
-        <div class="container">
+        <div class="container-fluid">
             <h2
                 class="font-titillium font-black text-oldLace text-2xl md:text-4xl lg:text-6xl mb-5"
             >
                 Projects
             </h2>
-            <div>
+            <div class="flex flex-row flex-1 overflow-scroll p-0">
                 <ProjectCards />
             </div>
         </div>
